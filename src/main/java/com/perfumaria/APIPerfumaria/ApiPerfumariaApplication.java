e4a8bffdf8de82d9688e36c7e437c71f92aee48e
wss124 <wellyson123443@gmail.com>
2024-12-10 10:09:41 -0300

    first commit

---
+++ src/main/java/com/perfumaria/APIPerfumaria/ApiPerfumariaApplication.java
@@ -0,0 +1,13 @@
+package com.perfumaria.APIPerfumaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPerfumariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPerfumariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfumaria/APIPerfumaria/ApiPerfumariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfumaria.APIPerfumaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPerfumariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
